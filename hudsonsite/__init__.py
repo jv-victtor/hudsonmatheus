de4dd6767c330d6a3de2103ae3c2d624b907152c
Victor <victor.costa1@escolar.ifrn.edu.br>
2026-01-07 14:10:21 -0300

    primeiro_comit

---
+++ hudsonsite/__init__.py
@@ -0,0 +1,1 @@
+# hudsonsite project package
---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def home(request):
+    context = {
+        'name': 'Hudson Matheus',
+        'rank': 'Faixa Marrom',
+        'quote': '"Disciplina, honra e respeito."',
+        'active': 'conquistas',
+    }
+    return render(request, 'main/index.html', context)
